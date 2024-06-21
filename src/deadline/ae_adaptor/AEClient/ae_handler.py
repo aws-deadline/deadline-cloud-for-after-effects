8d531b1a3d410278976899d0d28c2968b16926bf
Alex Hughes <ahughesalex@gmail.com>
2024-06-21 16:39:07 -0700

    Execute our render over the IPC not `aerender`
    The `start_render` call is now done over the IPC because it is a
    `listenBlocking` call and the `aerender.exe` executable will hang
    waiting for that blocking call to finish before starting its `aerender`
    call.
    
    Signed-off-by: Alex Hughes <ahughesalex@gmail.com>

--- src/deadline/ae_adaptor/AEClient/ae_handler.py
+++ src/deadline/ae_adaptor/AEClient/ae_handler.py
@@ -61,27 +61,16 @@ class AEHandler:
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
 
-        filename = self.output_pattern + "." + self.output_format
-        output = os.path.join(self.output_dir, filename)
-        ae_render_exe = os.environ.get("AFTEREFFECTS_ADAPTOR_AERENDER_EXECUTABLE", "aerender")
+        data.update({
+            "comp_name": self.comp_name,
+            "output_dir": self.output_dir,
+            "output_pattern": self.output_pattern,
+            "output_format": self.output_format,
 
-        subprocess.run(
-            [
-                ae_render_exe,
-                "-project",
-                self.file_path,
-                "-comp",
-                self.comp_name,
-                "-output",
-                output,
-                "-s",
-                str(frame),
-                "-e",
-                str(frame),
-            ]
-        )
-
-        print(f"AEClient: Finished Rendering Frame {frame}\n", flush=True)
+        })
+        resp = send_command("start_render", data)
+        logger.info("RESPONSE")
+        print(resp, flush=True)
 
     def set_output_file_path(self, data: dict) -> None:
         """
