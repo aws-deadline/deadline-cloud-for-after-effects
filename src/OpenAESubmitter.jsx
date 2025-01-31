8e8d45f65e2e600153c78fb33b814a5b48fa090d
Hao Zhao <36955586+eherozhao@users.noreply.github.com>
2025-01-31 15:01:41 -0800

    docs: update the warning messages if customers don't have scripts allowed in the AE
    Signed-off-by: Hao Zhao <36955586+eherozhao@users.noreply.github.com>

--- src/OpenAESubmitter.jsx
+++ src/OpenAESubmitter.jsx
@@ -36,18 +36,19 @@ if (isSecurityPrefSet()) {
     });
     errorText.graphics.foregroundColor = errorText.graphics.newPen(
         errorText.graphics.PenType.SOLID_COLOR,
-        [1.0, 0.2, 0.2],
+        [1.0, 1.0, 0.0],
         1
     );
-    errorText.text = "ERROR: Insufficient Script Permissions";
+    errorText.text = "Update Script Permissions";
     var errorText2 = root.add("statictext", undefined, "", {
         multiline: true,
     });
     errorText2.text = [
-        "Please allow script networking and file access:",
-        '  1)  Go to "Edit > Preferences > Scripting & Expressions"',
+        "In order for the Deadline Cloud submitter to execute, you need to update your script permissions to allow script networking and file access. To do this, follow the instructions below",
+        "  1)  For Windows User: Select Edit > Preferences > Scripting & Expressions > select Allow Scripts To Write Files And Access Network",
+        "       For macOS User: Select After Effects > Settings > Scripting & Expressions > select Allow Scripts To Write Files And Access Network",
         '  2)  Check "Allow Scripts to Write Files and Access Network"',
-        '  3)  (Optional) Deselect "Warn User When Executing Files"',
+        '  3)  (Optional) To disable warnings every time you submit a job with the submitter, you can deselect "Warn User When Executing Files"',
         "  4)  Close this window and try again.",
     ].join("\n");
     errorText2.alignment = ["fill", "fill"];
