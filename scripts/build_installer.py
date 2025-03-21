77289abefd021cd2e9e61364f9c6ebc555e2f2f3
Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>
2025-03-20 17:14:06 -0700

    chore: added comment for not needing to run depsBundle shell script
    Signed-off-by: Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>

--- scripts/build_installer.py
+++ scripts/build_installer.py
@@ -96,6 +96,14 @@ def build_installer(
     else:
         raise ValueError(f"Unknown platform '{installer_platform}'")
 
+    # Not needed for After Effects since it's a JavaScript-based submitter
+    # try:
+    #     deps_bundle_output = run(["bash", "depsBundle.sh"])
+    #     print(deps_bundle_output)
+    # except Exception as e:
+    #     print(f"Error when bundling dependencies: {e}")
+    #     raise
+
     install_builder_cli = install_builder_location / "bin" / "builder"
     out_dir = workdir / "out"
     installer_version = os.getenv("INSTALLER_VERSION") if not dev else "00000000"
