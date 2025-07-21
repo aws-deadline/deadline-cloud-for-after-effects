ad38f0e5fb5f1fc630b107cff2865da3bc0ebc50
Alex Hughes <ahughesalex@gmail.com>
2025-07-21 10:46:29 -0700

    Adding the enable/disable state for taskRun
    Signed-off-by: Alex Hughes <ahughesalex@gmail.com>

--- dist/DeadlineCloudSubmitter.jsx
+++ dist/DeadlineCloudSubmitter.jsx
@@ -2881,6 +2881,10 @@ function buildUI(thisObj) {
         framesPerTaskTextBox.enabled = false
         mfrCheckBox.enabled = false
         maxCpuUsagePercentageTextBox.enabled = false
+        taskRunCheckbox.enabled = false
+        taskRunDaysInput.enabled = false
+        taskRunHoursInput.enabled = false
+        taskRunMinutesInput.enabled = false
 
         if (selection.length !== 1) {
             return
@@ -2891,6 +2895,10 @@ function buildUI(thisObj) {
         framesPerTaskTextBox.enabled = imageOutput
         mfrCheckBox.enabled = true
         maxCpuUsagePercentageTextBox.enabled = true
+        taskRunCheckbox.enabled = true
+        taskRunDaysInput.enabled = true
+        taskRunHoursInput.enabled = true
+        taskRunMinutesInput.enabled = true
 
         logger.debug("    Setting framesPerTaskTextBox.text to: " + selectionItem.subItems[1].text);
         framesPerTaskTextBox.text = selectionItem.subItems[1].text
@@ -2911,6 +2919,14 @@ function buildUI(thisObj) {
         maxCpuUsagePercentageTextBox.text = settings.maxCpuUsagePercentage()
 
         maxCpuUsagePercentageTextBox.enabled = mfrCheckBox.value
+
+        taskRunCheckbox.value = settings.taskRunTimeout()
+        taskRunDaysInput.enabled = taskRunCheckbox.value
+        taskRunDaysInput.text = settings.taskRunDays()
+        taskRunHoursInput.enabled = taskRunCheckbox.value
+        taskRunHoursInput.text = settings.taskRunHours()
+        taskRunMinutesInput.enabled = taskRunCheckbox.value
+        taskRunMinutesInput.text = settings.taskRunMinutes()
     }
 
     list.onChange = onSelectionChange;
