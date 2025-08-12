f539664dab81583c79994c9e4438fae72b84a21c
Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>
2025-08-12 14:00:05 -0700

    chore: added missing semicolon
    Signed-off-by: Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>

--- src/utils/Utils.jsx
+++ src/utils/Utils.jsx
@@ -963,7 +963,7 @@ function __generateUtil() {
     }
 
     function getXMPPathLeaf(path) {
-        const regex = /xmp:([^\/]+)$/
+        const regex = /xmp:([^\/]+)$/;
         return regex.exec(path)[1];
     }
 
