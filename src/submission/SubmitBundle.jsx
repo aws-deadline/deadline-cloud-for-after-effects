9abe248d0cb33d8a0afa2725d1f53b58920a5b2a
Jaikish Nunna <jaikish@amazon.com>
2025-10-14 08:17:51 -0700

    fix: update generatePrettyName as well to be consistent to replace special characters
    Signed-off-by: Jaikish Nunna <jaikish@amazon.com>

--- src/submission/SubmitBundle.jsx
+++ src/submission/SubmitBundle.jsx
@@ -120,6 +120,8 @@ function generatePrettyParameterName(index, compName, parameter) {
     if (compName.length >= maxLength) {
         compName = compName.substring(0, maxLength - 3) + "...";
     }
+    // replace non-alphanumeric characters to comply with OpenJD
+    compName = compName.replace(/[^a-zA-Z0-9]/g, '_');
     return "(" + index + "_" + compName + ") " + parameter;
 }
 
